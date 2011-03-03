9c4658902fe2d3758e86858616762fbc221e66f0
jscheier <none@none>
2011-03-02 17:47:27 -0800

    Import .Net implementation from the C++ DLL.

--- cplusplus/pythoncppdll.cpp
+++ cplusplus/pythoncppdll.cpp
@@ -3,10 +3,12 @@
 
 #include "stdafx.h"
 
+using namespace dotnettoolfrompython;
+
 extern "C"
 {
 
-  __declspec(dllexport) int AddAreaFieldToFeatureClass(const wchar_t* feature_class, const wchar_t* field_name)
+  __declspec(dllexport) int AddAreaFieldToFeatureClassCPlusPlus(const wchar_t* feature_class, const wchar_t* field_name)
   {
 	  // Convert char*s to bstring
 	  _bstr_t catalogPath(feature_class),
@@ -71,4 +73,11 @@ extern "C"
   }
 
 
+  __declspec(dllexport) int AddAreaFieldToFeatureClassCSharp(const wchar_t* feature_class, const wchar_t* field_name)
+  {
+    ExecuteTool et;
+    System::String ^fc_string(gcnew System::String(feature_class)),
+                   ^field_name_string(gcnew System::String(field_name));
+    return et.AddAreaFieldToFeatureClass(fc_string, field_name_string);
+  }
 }
